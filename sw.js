17e9779e7fa1a9b678a1950e6b38bb9691dd9ac6
Thalapath <reelsrundown@gmail.com>
2026-03-28 15:19:53 +0530

    Add service worker to cache application assets
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'thalappanth-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/game/assets/tap.png.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
